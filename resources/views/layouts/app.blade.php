d6e9940ff592e3f07753385f965b81a2a44f14fa
Kim Larsson <kim@bravissimo.se>
2018-01-09 10:57:17 +0100

    EP12

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,13 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        <li><a href="{{ url('/') . '/threads' }}">All Threads</a></li>
+                        <li>
+                            <a href="{{ url('/') . '/threads' }}">All Threads</a>
+                        </li>
+
+                        <li>
+                            <a href="{{ url('/') . '/threads/create' }}">New Thread</a>
+                        </li>
 
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Channels <span class="caret"></span></a>
--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -10,16 +10,40 @@
                 <div class="panel-body">
                     <form class="" action="{{ url('/') . '/threads' }}" method="POST">
                         {{ csrf_field() }}
-                        
+
+                        <div class="form-group">
+                            <label for="channel_id">Choose a Channel:</label>
+                            <select class="form-control" id="channel_id" name="channel_id" required>
+                                <option value="">Choose one...</option>
+                                @foreach (App\Channel::all() as $channel)
+                                    <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>
+                                        {{ $channel->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
                         <div class="form-group">
                             <label for="title">Title:</label>
-                            <input class="form-control" name="title" name="title">
+                            <input class="form-control" id="title" name="title" value="{{ old('title') }}" required>
                         </div>
+
                         <div class="form-group">
                             <label for="body">Body:</label>
-                            <textarea class="form-control" name="body" name="body" rows="5"></textarea>
+                            <textarea class="form-control" id="body" name="body" rows="5" required>{{ old('body') }}</textarea>
                         </div>
-                        <button class="btn btn-primary" type="submit">Publish</button>
+
+                        <div class="form-group">
+                            <button class="btn btn-primary" type="submit">Publish</button>
+                        </div>
+
+                        @if (count($errors))
+                            <ul class="alert alert-danger list-unstyled">
+                                @foreach ($errors->all() as $key => $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        @endif
                     </form>
                 </div>
             </div>
